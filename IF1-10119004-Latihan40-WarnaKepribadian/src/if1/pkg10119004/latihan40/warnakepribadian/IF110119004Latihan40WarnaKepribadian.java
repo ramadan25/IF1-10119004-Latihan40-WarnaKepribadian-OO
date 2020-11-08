4c314218f4f1ca110bc1752e274d2804f932b7f9
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-11-08 12:36:42 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan40-WarnaKepribadian/src/if1/pkg10119004/latihan40/warnakepribadian/IF110119004Latihan40WarnaKepribadian.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author ENDOG
+ *  Nama      : Muhamad Ramadan
+ *  NIM       : 10119004
+ *  Kelas     : IF1
+ *  Deskripsi : menampilkan warna kepribadian
+ */
+public class IF110119004Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKepribadian kepribadian = new WarnaKepribadian();
+        kepribadian.inputWarna();
+        kepribadian.inputNama();
+        kepribadian.cekKepribadian();
+    }
+    
+}
